dfba0715e945a8f9b750c05f1f3728276d39a94d
Junhao Li <streaver91@gmail.com>
2015-09-17 19:24:54 -0400

    vme

--- te/PotentialFittingCompare/fitting.py
+++ te/PotentialFittingCompare/fitting.py
@@ -4,58 +4,73 @@ Fitting EPID Potential With Python Minimization
 Date: 2015/09/12
 Author: Junhao Li
 """
+import ase
+from ase import Atoms
+from ase.lattice import bulk
+
+from kimcalculator import KIMCalculator
+import kimservice as km
 
 import numpy as np
-import scipy as sp
+from scipy import optimize
 import ctypes
 import os
 from copy import copy
-import ase
-from ase import Atoms
-from kimcalculator import KIMCalculator
-import kimservice as km
 import sys
+import math
+import random
 
+LATTICE = 'diamond'
+ELEM = 'Si'
 MODEL_DIR = '/home/openkim/openkim-repository/mo'
 MODEL = 'EDIP_BOP_Bazant_Kaxiras_Si__MO_958932894036_001'
 INPUT_FILE = 'forces_small.txt'
-INITIAL_PARAM = {
-    'PARAM_FREE_A': 0.99,
-    # ...
-}
-
-paramNames = []
+PARAMS_FILE = 'params.txt'
+FORCE_WEIGHT = 0.9
+ENERGY_WEIGHT = 0.1
 
 def getParamNames():
+    paramNames = []
     desFile = os.path.join(MODEL_DIR, MODEL, 'descriptor.kim')
     with open(desFile, 'r') as f:
         for line in f:
             if 'PARAM_FREE' in line:
                 paramName = line.split()[0]
                 paramNames.append(paramName)
+    return paramNames
 
-def setZeroInitial():
-    for name in paramNames:
-        INITIAL_PARAM[name] = 0
-    print INITIAL_PARAM
+def readParams():
+    # For debug
+    paramString = '3.1213820	7.9821730	1.5075463	1.2085196	0.5774108	1.4533108	1.1247945	3.1213820	2.5609104	0.6966326	312.1341346	0.2523244	0.0070975	3.1083847	-0.165799	32.557	0.286198	0.66'
+    paramValues = paramString.split('\t')
+    paramDict = {}
+    for i in range(len(paramValues)):
+        paramValues[i] = float(paramValues[i]) * random.uniform(0.9, 1.1)
+    return paramValues
 
 def readData():
+    # Read Data in Format:
+    # energy (eV)
+    # cellSize (angstrom)
+    # x y z Fx Fy Fz
+    # ...
+    positions = []
+    forces = []
+    energy = 0
+    cellSize = 0
     with open(INPUT_FILE, 'r') as f:
         energy = float(f.readline())
         cellSize = float(f.readline())
-        positions = []
-        forces = []
         for line in f:
             line = line.split()
             if len(line) == 6:
                 for i in range(6):
                     line[i] = float(line[i])
-                # print line
                 positions.append([line[0], line[1], line[2]])
                 forces.append([line[3], line[4], line[5]])
     nAtoms = len(positions)
     atoms = Atoms(
-        'Si' + str(nAtoms),
+        ELEM + str(nAtoms),
         positions = positions,
         cell = (cellSize, cellSize, cellSize),
         pbc = (1, 1, 1),
@@ -66,26 +81,27 @@ def readData():
     calc.set_ghosts(ghosts)
     return atoms, forces, energy
     
-def getResiduals(params):
-    calc = KIMCalculator(MODEL)
-    atoms.set_calculator(calc)
-    for name in params:
-        pkim = calc.pkim
-        p = km.KIM_API_get_data_double(calc.pkim, name)
-        print p
-        raw_input("Press Enter to continue...")
-        # calc.free_kim()
-        # km.KIM_API_set_data_double(pkim, name, [])
-        km.KIM_API_model_reinit(calc.pkim)
-        # km.KIM_API_set_data_double(pkim, name, [hex(id(1.1))])
-        p = km.KIM_API_get_data_double(pkim, name)
-        print p
+def getResiduals(params, paramNames, atoms, forces, energy):
+    nAtoms = atoms.get_number_of_atoms()
+    pkim = atoms.calc.pkim
+    for i in range(len(paramNames)):
+        p = km.KIM_API_get_data_double(pkim, paramNames[i])
+        p[0] = params[i]
+        # print p
+    km.KIM_API_model_reinit(pkim)
+    tmpForces = atoms.get_forces()
+    sumForce = sum(np.linalg.norm(tmpForces[i] - forces[i]) for i in range(nAtoms))
+    print 'Force:', sumForce
+    return sumForce * FORCE_WEIGHT + energy * ENERGY_WEIGHT
 
-    return 0
-
-getParamNames()
-# setZeroInitial()
-atoms, forces, energy = readData()
-getResiduals(INITIAL_PARAM)
-# res = sp.optimize.leastsq(getResiduals, INITIAL_PARAM, full_output = 1)
-# print res
+def main():
+    paramNames = getParamNames()
+    print paramNames
+    initialParams = readParams()
+    print initialParams
+    atoms, forces, energy = readData()
+    getResiduals(initialParams, paramNames, atoms, forces, energy)
+    # res = optimize.fmin(getResiduals, initialParams, args = (paramNames, atoms, forces, energy), full_output = 1)
+    # print res
+    
+main()
